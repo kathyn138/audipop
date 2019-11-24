aa0e8dd3419a2ebe3a790dc1855768b382f277f7
Kathy Nguyen <kathyn262@gmail.com>
2019-11-23 19:03:37 -0800

    added positioning for circles

--- src/Circle.js
+++ src/Circle.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import './Circle.css';
 
 class Circle extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      visiblity: true
     }
+
+    // this.interval = null;
   }
+
+  // componentDidMount() {
+  //   this.interval = setTimeout(() => this.setState({ visibility: true }), 3000);
+  // }
+
+  // componentWillUnmount() {
+  //   clearInterval(this.interval);
+  // }
 
   render() {
     return (
-      <div className="circle" id={this.props.id}></div>
+      <div className={`circle-${this.props.id}`} ></div>
     )
   }
 }
