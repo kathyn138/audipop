cd1b495fa0e79ec3d83147c6a35ee3d3b02ba61b
Kathy Nguyen <kathyn262@gmail.com>
2019-11-25 21:36:16 -0800

    made tick and untick stop
    both stop when there are no more circle positions. untick checks circles state length bc need to remove all circles from screen

--- src/Board.js
+++ src/Board.js
@@ -5,7 +5,8 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      circles: []
+      circles: [],
+      circlePositions: [1, 2, 3, 4, 2, 3, 1, 4, 2, 3, 1, 2, 3, 2, 4, 1, 2]
     }
     // this.randomNumGenerator = this.randomNumGenerator.bind(this);
     // this.tick = this.tick.bind(this);
@@ -13,50 +14,59 @@ class Board extends React.Component {
 
   componentDidMount() {
     this.timerID = setInterval(
-      () => this.tick(), 
-      2000
+      () => this.tick(),
+      1000
     );
-    this.interval = setInterval(
-      () => this.untick(), 4000);
+    this.timerRemove = setInterval(
+      () => this.untick(), 2000);
   }
 
   componentWillUnmount() {
     clearInterval(this.timerID);
-  }
-  
-  randomNumGenerator() {
-    return Math.floor(Math.random() * Math.floor(6))
+    clearInterval(this.timerRemove);
   }
 
-  tick () {
-    let circlePosition = this.randomNumGenerator();
-    console.log(circlePosition)
+  // randomNumGenerator() {
+  //   return Math.floor(Math.random() * Math.floor(6));
+  // }
+
+  tick() {
+    // let circlePositions = [1, 2, 3, 4, 2, 3, 1, 4, 2, 3, 1, 2, 3, 2, 4, 1, 2];
+    // let position = circlePositions.shift();
     // let circlesArr = [];
     // let selectedCircle;
-    
+
     // for (let i = 0; i < numCircles; i++) {
     //   selectedCircle = this.randomNumGenerator();
     //   circlesArr.push(selectedCircle);
     // }
-
-    this.setState({
-      circles: [...this.state.circles, circlePosition]
-    });
+    if (this.state.circlePositions.length > 0) {
+      let shiftedCirclePositions = [...this.state.circlePositions.slice(1)]
+      this.setState({
+        circles: [...this.state.circles, this.state.circlePositions[0]],
+        circlePositions: shiftedCirclePositions
+      });
+    }
   }
 
   untick() {
-    let shiftArr = this.state.circles.filter(id => id !== this.state.circles[0])
-    this.setState({
-      circles: shiftArr
-    })
+    if (this.state.circles.length > 0) {
+      let shiftArr = this.state.circles.filter(id => id !== this.state.circles[0])
+      this.setState({
+        circles: shiftArr
+      })
+    }
 
   }
   // need to pass in a sort of idx and not empty array in state
 
   render() {
-    console.log(this.state.circles)
+    console.log(this.state.circlePositions)
+    console.log('length', this.state.circlePositions.length)
     return (
-      this.state.circles.map((c) => <Circle id={c}/>)
+      <div className="board">
+        {this.state.circles.map(c => <Circle id={c} />)}
+      </div>
     )
     // return (
     //   <Circle id="1"/>
