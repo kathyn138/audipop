253ad09d1a6f076b456310e3164d6343d5e6a915
Kathy Nguyen <kathyn262@gmail.com>
2019-11-27 15:27:50 -0800

    added pregame countdown

--- src/CountDown.js
+++ src/CountDown.js
@@ -2,13 +2,6 @@ import React from 'react';
 import './CountDown.css';
 
 class CountDown extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-
   render() {
     return (
         <div className="count">{this.props.count}</div>
