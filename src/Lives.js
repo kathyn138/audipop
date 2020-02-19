e0f348793a72d6281856957976e5b4cc360b1f17
Kathy Nguyen <kathyn262@gmail.com>
2020-02-19 15:01:45 -0800

    added poro lives

--- src/Lives.js
+++ src/Lives.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Lives.css';
+import poroLife from './assets/poro-life.png';
 
 class Lives extends React.Component {
   constructor(props) {
@@ -11,7 +12,12 @@ class Lives extends React.Component {
 
   render() {
     return (
-      <div className="lives">Lives: {this.props.lives}</div>
+      <div className="lives">
+        <img src={poroLife} className="poro-life" alt="life"></img>
+        <img src={poroLife} className="poro-life" alt="life"></img>
+        <img src={poroLife} className="poro-life" alt="life"></img>
+      </div>
+      // <div className="lives">Lives: {this.props.lives}</div>
     )
   }
 }
